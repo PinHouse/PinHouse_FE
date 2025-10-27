aeb2522d870b50eacb5aa7b37616e0e54229f2de
JW_Ahn0 <jwahn@ivh.co.kr>
2025-10-27 17:53:39 +0900

    file: 카카오,네이버 아이콘 위치 변경

--- src/assets/icons/button/naverLogo.tsx
+++ src/assets/icons/button/naverLogo.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 
-export const KakaoLogo = (props: SVGProps<SVGSVGElement>) => {
+export const NaverLogo = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#clip0_8988_1942)">
