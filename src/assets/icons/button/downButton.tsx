cc89b0b28e30d5bc43e081139feb6890da75853d
JW-Ahn0 <jun.woo.an0@gmail.com>
2025-11-16 01:19:30 +0900

    fix: up,.down 아이콘 브라우저 콘솔에러 해결

--- src/assets/icons/button/downButton.tsx
+++ src/assets/icons/button/downButton.tsx
@@ -3,13 +3,13 @@ import { SVGProps } from "react";
 export const DownButton = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_9421_1045)">
+      <g clipPath="url(#clip0_9421_1045)">
         <path
           d="M6 9L12 15L18 9"
           stroke="#9F9FAB"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
