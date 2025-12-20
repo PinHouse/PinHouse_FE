1967cb3306abddafb8a0b356dd602b7235bf1706
JW-Ahn0 <jun.woo.an0@gmail.com>
2025-12-21 07:18:03 +0900

    feat: 자격진단 공통컴포넌트 Input-숫자설정관련

--- src/features/eligibility/ui/common/index.ts
+++ src/features/eligibility/ui/common/index.ts
@@ -10,3 +10,9 @@ export type { EligibilityHelpButtonProps } from "./eligibilityHelpButton";
 
 export { EligibilityPriceInput } from "./eligibilityPriceInput";
 export type { EligibilityPriceInputProps } from "./eligibilityPriceInput";
+
+export { EligibilityNumberInputList } from "./eligibilityNumberInputList";
+export type {
+  EligibilityNumberInputListProps,
+  EligibilityNumberInputOption,
+} from "./eligibilityNumberInputList";
